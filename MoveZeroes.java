dd7bd7c2771d6f7dfc3931dfef1e2cab86799307
Arnab Chowdhury <101791351+arnab-1156@users.noreply.github.com>
2023-07-05 03:39:18 +0530

    Create MoveZeroes.java

---
+++ MoveZeroes.java
@@ -0,0 +1,12 @@
+class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int x = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] != 0)
+                nums[x++] = nums[i];
+        }
+        while(x < n)
+            nums[x++] = 0;
+    }
+}
